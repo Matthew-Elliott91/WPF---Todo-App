8cf9aa5a1899630f0d4134d14e8a11f9b1ffd709
Matthew Elliott <m.elliott091091@gmail.com>
2025-01-10 16:10:01 +0000

    Created grid, practiced creating elements with Csharp, added a click and button press handler, added the scrollview and finished adding the todo creation logic

--- Todo App/MainWindow.xaml.cs
+++ Todo App/MainWindow.xaml.cs
@@ -20,5 +20,52 @@ namespace Todo_App
         {
             InitializeComponent();
         }
+
+        int i = 0;
+        // Create an event handler for the AddTodoButton
+        private void AddTodoButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Store the input text in a variable
+            string todoText = TodoInput.Text;
+
+            // Check if the input text is not empty
+            if (todoText.Length > 0)
+            {
+                
+
+                i++;
+                // Create a new TextBlock element
+                TextBlock todoItem = new TextBlock
+                {
+                    Text = $"{i}. {todoText}",
+                    Margin = new Thickness(10),
+                    Foreground = new SolidColorBrush(Colors.White),
+                };
+                // Add the new TextBlock element to the TodoList
+                TodoList.Children.Add(todoItem);
+
+                // Clear the input text
+                TodoInput.Text = "";
+
+                // Focus on the input text
+                TodoInput.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a todo item.");
+            }
+            
+        }
+
+        // Create an event handler for the enter key
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                // Routing the event to the AddTodoButton_Click event handler
+                RoutedEventArgs keyPressEnterEventArgs = new RoutedEventArgs(Button.ClickEvent);
+                AddTodoButton_Click(sender, keyPressEnterEventArgs);
+            }
+        }
     }
 }
